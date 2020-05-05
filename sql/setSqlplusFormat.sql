099f8334a205cbfc361e805ff47f403dbe44a45b
ssskming@126.com <ssskming@126.com>
2020-05-05 16:16:41 +0800

    迁移使用脚本

---
+++ sql/setSqlplusFormat.sql
@@ -0,0 +1,16 @@
+
+
+set echo off;
+set feedback off;
+set heading off;
+set pagesize 0;
+set linesize 1000;
+set termout off;
+set trimout on;
+set trimspool on;
+set newpage 0;
+set space 0;
+set verify off;
+set markup html off spool off;
+set long 2048576;
+set trim on;
