bb7047a158d40538c541087d3c7ef5c9db1b9e21
ssskming@126.com <ssskming@126.com>
2020-05-05 21:14:34 +0800

    自动化安装fi客户端

--- shell/autoInstallFI.sh
+++ shell/autoInstallFI.sh
@@ -6,4 +6,24 @@ set ip [lindex $argv 0]
 
 spawn ssh $ip
 
-expect 
+expect {
+	"yes/no" {send "yes\r"}
+	} 
+
+expect "]$"
+send "cd /tmp/fi_clinet\r"
+
+expect "]$"
+send "./install.sh /opt/fi | tee -a ./install.log\r"
+
+expect "]$"
+send "scp ..."
+
+expect {
+	"yes/no" {send "yes\r";exp_continue}
+	"]$" {send "source ... && sh .... | tee -a ./install.log\r"}
+	}
+
+expect "]$"
+send "exit\r"
+expect eof
